3772d9f8133aa03c4a4bfd6fb62b267aaa82aba6
uakora <mEnternityevoL@gmail.com>
2018-11-27 22:52:31 +0800

    database learn01

--- src/PluginSpace/urls.py
+++ src/PluginSpace/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('index/', views.plugin_space_index, name='index'),
+    path('datatable/learn01/', views.datatable_learn01, name='datatable'),
 ]
--- src/PluginSpace/views.py
+++ src/PluginSpace/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def plugin_space_index(request):
     return render(request, 'index.html')
+
+
+def datatable_learn01(request):
+    return render(request, 'datatables/learn01.html')
