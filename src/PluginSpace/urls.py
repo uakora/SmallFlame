b9a8207f9660871b3b9a7f80b080a06477c7196e
uakora <mEnternityevoL@gmail.com>
2018-12-06 23:10:10 +0800

    bbb

--- src/PluginSpace/urls.py
+++ src/PluginSpace/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('index/', views.plugin_space_index, name='index'),
     path('datatable/learn01/', views.datatable_learn01, name='datatable'),
+    path('datatable/learn02/', views.datatable_learn02, name='datatable'),
+    path('datatable/learn03/', views.datatable_learn03, name='datatable'),
 ]
--- src/PluginSpace/views.py
+++ src/PluginSpace/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 def plugin_space_index(request):
     return render(request, 'index.html')
 
-
 def datatable_learn01(request):
     return render(request, 'datatables/learn01.html')
+
+def datatable_learn02(request):
+    return render(request, 'datatables/learn02.html')
+
+def datatable_learn03(request):
+    return render(request, 'datatables/learn03.html')
