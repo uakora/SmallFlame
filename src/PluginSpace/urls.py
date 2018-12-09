66d21ff9a568b8887b98c8dacc041eaa90c2d955
uakora <mEnternityevoL@gmail.com>
2018-12-09 23:17:08 +0800

    搜索练习

--- src/PluginSpace/urls.py
+++ src/PluginSpace/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('index/', views.plugin_space_index, name='index'),
     path('datatable/learn01/', views.datatable_learn01, name='datatable'),
     path('datatable/learn02/', views.datatable_learn02, name='datatable'),
+    path('datatable/learn02/data/', views.datatable_learn02_data, name='datatable'),
     path('datatable/learn03/', views.datatable_learn03, name='datatable'),
 ]
--- src/PluginSpace/views.py
+++ src/PluginSpace/views.py
@@ -1,5 +1,7 @@
+import random
+import json
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def plugin_space_index(request):
     return render(request, 'index.html')
@@ -8,7 +10,38 @@ def datatable_learn01(request):
     return render(request, 'datatables/learn01.html')
 
 def datatable_learn02(request):
-    return render(request, 'datatables/learn02.html')
+    table_head = ['姓名', '性别', '角色', '全年', '月均',
+                  'Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'Jun.',
+                  'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.']
+    return render(request, 'datatables/learn02.html', {'table_head': table_head})
+def datatable_learn02_data(request):
+    names = ['Abbie', 'Acacia', 'Adalia', 'Adelaide', 'Adonia', 'Amber', 'Antonia', 'Aurora', 'Azura', 'Alysa', 'Akili',
+             'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie',
+             'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie',
+             'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie',
+             'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie', 'Abbie']
+    sex_enum = ['男', '女']
+    role_enum = ['LM', 'PL', 'SE', 'MDE', 'DEVELOPER', 'TEST']
+    month_enum = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.']
+    data = []
+    for name in names:
+        code_enum = []
+        year_total = 0
+        for i in range(len(month_enum)):
+            month_code = random.randint(0, 3500)
+            code_enum.append(month_code)
+            year_total += month_code
+        month_avg = year_total//len(month_enum)
+        item = {}
+        item['name'] = name
+        item['sex'] = sex_enum[random.randint(0, 1)]
+        item['role'] = role_enum[random.randint(0, 5)]
+        item['year_total'] = year_total
+        item['month_avg'] = month_avg
+        item['code_enum'] = code_enum
+
+        data.append(item)
+    return HttpResponse(json.dumps({'data': data}))
 
 def datatable_learn03(request):
     return render(request, 'datatables/learn03.html')
