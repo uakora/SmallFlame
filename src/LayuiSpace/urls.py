2206c069915d41891731a794cf53dd82d1eaf4ba
uakora <mEnternityevoL@gmail.com>
2019-05-30 23:51:56 +0800

    layui learn

--- src/LayuiSpace/urls.py
+++ src/LayuiSpace/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.layui_space_index, name='layui'),
-
+    path('index/', views.layui_space_index),
+    path('demo/<int:demo_idx>', views.layui_demo_idx),
 ]
--- src/LayuiSpace/views.py
+++ src/LayuiSpace/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def layui_space_index(request):
     return render(request, 'layui_space/index.html')
+
+
+def layui_demo_idx(reques, demo_idx=1):
+    html = 'layui_space/demo_%d.html' % (demo_idx)
+    return render(reques, html, {"demo_idx": demo_idx})
